ce1aaca1788af94ea581c28f0eaed6cab4ee14d7
Emre Ergun <emreergun@engramsoft.com>
2025-04-24 17:34:53 +0300

    first endpoint added for health check. actix-web used for web framework. tokio for async runtime

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-fn main() {
-    println!("Hello, world!");
-    println!("Added to test main branch merge protection")
+//! src/main.rs
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
