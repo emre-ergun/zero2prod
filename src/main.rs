1ac96500ded27e852ac932e4634ccf58daeed95e
Emre Ergun <emreergun@engramsoft.com>
2025-04-25 16:07:41 +0300

    test of health_check improved. Random port used for the test with the help of TcpListener from std library. A listener created and provided to the run function as an argument which help us to test our application separately.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 //! src/main.rs
 
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind");
+    run(listener)?.await
 }
