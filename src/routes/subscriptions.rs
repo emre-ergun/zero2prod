e3ac8647d8a8a42f1eee3b123061e2eea7116bed
Emre Ergun <emreergun@engramsoft.com>
2025-04-26 16:27:04 +0300

    logger added to trace logs. cargo test will fail

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,6 +4,7 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -13,6 +14,17 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding new subscriber",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+
+    let _ = request_span.enter();
+    let query_span = tracing::info_span!("Saving new subscriber in the database");
+
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -24,11 +36,15 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("New subscriber details have been saved");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute the query: {:#?}", e);
+            tracing::error!("Failed to execute the query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@
 
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -13,6 +14,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     // Capture the connection from the surrounded environment
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // register the connnection as part of the application state
