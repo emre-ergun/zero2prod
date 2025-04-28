6126dd4061dce2daed0552247794a380f50125ac
Emre Ergun <emreergun@engramsoft.com>
2025-04-28 13:42:54 +0300

    request id provided by tracing_actix_web crate, function specific request id removed. Secrecy added to create secret for important data like password

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 //! src/main.rs
 
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -12,9 +13,10 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Db");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Db");
 
     // We have removed the hard-coded `8000` - it's now coming from our settings!
     let address = format!("127.0.0.1:{}", configuration.application_port);
