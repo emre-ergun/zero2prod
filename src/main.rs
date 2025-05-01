81fd8c9ab6282a50126725839ccb25edf693ec32
Emre Ergun <emreergun@engramsoft.com>
2025-05-01 18:17:53 +0300

    email_client service added. reqwest used to request to the mail server. wiremock used to create mock http server to test email_client functionality

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
+use zero2prod::email_client::EmailClient;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
@@ -14,6 +15,19 @@ async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
 
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email");
+
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
+
     // We have removed the hard-coded `8000` - it's now coming from our settings!
     let address = format!(
         "{}:{}",
@@ -21,5 +35,5 @@ async fn main() -> Result<(), std::io::Error> {
     );
     let listener = TcpListener::bind(address)?;
 
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,23 @@
 //! src/startup.rs
 
+use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
-pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+    email_client: EmailClient,
+) -> Result<Server, std::io::Error> {
     // wrap the connection with the smart pointer
-    let db_pool = web::Data::new(db_pool);
-
+    let db_pool = Data::new(db_pool);
+    // wrap the email client with the smart pointer
+    let email_client = Data::new(email_client);
     // Capture the connection from the surrounded environment
     let server = HttpServer::new(move || {
         App::new()
@@ -19,6 +26,8 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
             .route("/subscriptions", web::post().to(subscribe))
             // register the connnection as part of the application state
             .app_data(db_pool.clone())
+            // register the emmail client as part of the application state
+            .app_data(email_client.clone())
     })
     .listen(listener)?
     .run();
