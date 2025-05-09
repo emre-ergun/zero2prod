c81fd0448232ff9fd351b23bdda6b9a2407a9a7e
Emre Ergun <emreergun@engramsoft.com>
2025-05-09 17:17:56 +0300

    login completed with the cookie settings from actix-web

--- src/startup.rs
+++ src/startup.rs
@@ -5,9 +5,13 @@ use crate::email_client::EmailClient;
 use crate::routes::{
     confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
 };
+use actix_web::cookie::Key;
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
+use actix_web_flash_messages::storage::CookieMessageStore;
+use actix_web_flash_messages::FlashMessagesFramework;
+use secrecy::ExposeSecret;
 use secrecy::Secret;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
@@ -81,10 +85,14 @@ pub fn run(
     // wrap the email client with the smart pointer
     let email_client = Data::new(email_client);
     let base_url = Data::new(ApplicationBaseUrl(base_url));
+    let message_store =
+        CookieMessageStore::builder(Key::from(hmac_secret.expose_secret().as_bytes())).build();
+    let message_framework = FlashMessagesFramework::builder(message_store).build();
     let hmac_secret = Data::new(HmacSecret(hmac_secret));
     // Capture the connection from the surrounded environment
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(message_framework.clone())
             .wrap(TracingLogger::default())
             .route("/", web::get().to(home))
             .route("/login", web::get().to(login_form))
